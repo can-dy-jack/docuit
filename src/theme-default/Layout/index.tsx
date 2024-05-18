24f4fe4aeb5515806eeb33b17d839d4e61a5edca
can-dy-jack <jjack_chen3@163.com>
2024-05-18 12:35:01 +0800

    配置文件

--- src/theme-default/Layout/index.tsx
+++ src/theme-default/Layout/index.tsx
@@ -4,7 +4,7 @@ export function Layout() {
   const [count, setCount] = useState(0);
   return (
     <div>
-      <h1>This is Layout Component 23333</h1>
+      <h1>This is Layout Component</h1>
       <div>
         {count}
         <button onClick={() => setCount(count + 1)}>Add Count</button>
